ec4000b64bbe5f5c84b0f3f606d39b0110d540e2
Angel Angelov <aangeloo@gmail.com>
2020-09-11 13:14:50 +0200

    explicit location of rmd file to wd

--- bin/seqtools.R
+++ bin/seqtools.R
@@ -10,11 +10,12 @@ require(rmarkdown)
 args <- commandArgs(trailingOnly = T)
 
 # render the rmarkdown, using fastq-report.Rmd as template
-rmarkdown::render(input = "fastq-stats-report.Rmd", 
+rmarkdown::render(input = file.path(getwd(), "fastq-stats-report.Rmd"), 
 									output_file = "fastq-stats-report.html", 
 									output_dir = getwd(), # important when knitting in docker 
 									knit_root_dir = getwd(), # important when knitting in docker 
-									params = list(fqfiles = args))
+									params = list(fqfiles = args)
+									)
 
 # this solved the seqfault error
 # https://github.com/cgpu/gel-gwas/commit/2c5a4e5e216478c4a0cbe869c8b4e437b333b787#diff-3254677a7917c6c01f55212f86c57fbf
