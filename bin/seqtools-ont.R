6eba99a45d3fbec64915f21ff45ee25c9307bd05
angelovangel <aangeloo@gmail.com>
2022-02-13 21:10:37 +0200

    report polish, explicit docker platform

--- bin/seqtools-ont.R
+++ bin/seqtools-ont.R
@@ -1,7 +1,8 @@
 #!/usr/bin/env Rscript
 #============
 #
-# this just renders the fastq-stats-report.Rmd file, passing the fastq files as params
+# this just renders the fastq-stats-report.Rmd file,
+# passing the fastq files as params
 # everything else is done there
 #
 #============
@@ -11,13 +12,12 @@ args <- commandArgs(trailingOnly = T)
 
 cat(str(args))
 # render the rmarkdown, using fastq-report.Rmd as template
-# cat(getwd(), "currentdir.txt")
-rmarkdown::render(input = "fastq-stats-report-ont.Rmd", 
-									output_file = "fastq-stats-report-ont.html", 
-									output_dir = getwd(), # important when knitting in docker 
-									knit_root_dir = getwd(), # important when knitting in docker 
-									params = list(fqfiles = args)
-									)
+rmarkdown::render(input = "fastq-stats-report-ont.Rmd",
+ output_file = "fastq-stats-report-ont.html",
+ output_dir = getwd(), # important when knitting in docker
+ knit_root_dir = getwd(), # important when knitting in docker
+ params = list(fqfiles = args)
+ )
 
 # this solved the seqfault error
 # https://github.com/cgpu/gel-gwas/commit/2c5a4e5e216478c4a0cbe869c8b4e437b333b787#diff-3254677a7917c6c01f55212f86c57fbf
--- bin/seqtools.R
+++ bin/seqtools.R
@@ -1,7 +1,8 @@
 #!/usr/bin/env Rscript
 #============
 #
-# this just renders the fastq-stats-report.Rmd file, passing the fastq files as params
+# this just renders the fastq-stats-report.Rmd file,
+# passing the fastq files as params
 # everything else is done there
 #
 #============
@@ -10,13 +11,12 @@ require(rmarkdown)
 args <- commandArgs(trailingOnly = T)
 
 # render the rmarkdown, using fastq-report.Rmd as template
-# cat(getwd(), "currentdir.txt")
-rmarkdown::render(input = "fastq-stats-report.Rmd", 
-									output_file = "fastq-stats-report.html", 
-									output_dir = getwd(), # important when knitting in docker 
-									knit_root_dir = getwd(), # important when knitting in docker 
-									params = list(files = args)
-									)
+rmarkdown::render(input = "fastq-stats-report.Rmd",
+ output_file = "fastq-stats-report.html",
+ output_dir = getwd(), # important when knitting in docker
+ knit_root_dir = getwd(), # important when knitting in docker
+ params = list(files = args)
+ )
 
 # this solved the seqfault error
 # https://github.com/cgpu/gel-gwas/commit/2c5a4e5e216478c4a0cbe869c8b4e437b333b787#diff-3254677a7917c6c01f55212f86c57fbf
